0cbc0f1b0459f787ee9f15a3dc7181344218b6b5
Three_Apple <73579842+ThreeApple0@users.noreply.github.com>
2022-03-15 10:32:11 +0900

    toast test

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -2,12 +2,30 @@ package com.example.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var toast:Toast = Toast.makeText(this.applicationContext,"",Toast.LENGTH_SHORT)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        bt1.setOnClickListener {
+            toast.cancel()
+            toast = Toast.makeText(this.applicationContext,"테스트 A",Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.BOTTOM,0,0);
+            toast.show()
+        }
+        bt2.setOnClickListener {
+            toast.cancel()
+            toast = Toast.makeText(this.applicationContext,"테스트 B",Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show()
+        }
     }
 }
